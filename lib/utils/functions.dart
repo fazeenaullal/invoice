57f88cc562b8e49cc4b8bde0873fe2fd271f9559
fazeenaullal <fazeenahusain@gmail.com>
2024-07-13 16:44:48 +0200

    final commit

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -6,8 +6,10 @@ import 'package:intl/intl.dart';
 
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 class Functions {
+
   static String formatDate(DateTime date) {
     final DateFormat formatter = DateFormat('yyyy-MM-dd');
     final String formatted = formatter.format(date);
@@ -17,15 +19,28 @@ class Functions {
   static void saveInvoice(
       {required String name,
       required Uint8List fileBytes,
-      String path = "/storage/emulated/0/Documents"}) async {
+      String path = "/storage/emulated/0/Download/"}) async {
     try {
+      final plugin = DeviceInfoPlugin();
+      final android = await plugin.androidInfo;
       bool checkPermission = await Permission.storage.request().isGranted;
-      if (checkPermission) {
+      final storageStatus = android.version.sdkInt < 33
+          ? await Permission.storage.request()
+          : PermissionStatus.granted;
+      if (storageStatus == PermissionStatus.granted) {
+        print("granted");
         File pdf_doc = File("$path/$name");
         await pdf_doc.writeAsBytes(fileBytes);
         Get.snackbar("done", "Invoice Saved succesfully to $path/$name",
             snackPosition: SnackPosition.BOTTOM);
-      } else {
+      } else if (checkPermission) {
+        File pdf_doc = File("$path/$name");
+        await pdf_doc.writeAsBytes(fileBytes);
+        Get.snackbar("done", "Invoice Saved succesfully to $path/$name",
+            snackPosition: SnackPosition.BOTTOM);
+      }
+      else {
+        print("denied");
         Get.snackbar("Error", "Storage permission denied !, please try again!",
             snackPosition: SnackPosition.BOTTOM);
         Future.delayed(
@@ -33,6 +48,9 @@ class Functions {
         );
         await Permission.storage.request();
       }
+
+
+
     } on FileSystemException catch (e) {
       Get.snackbar("ERROR", "${e.message} $path/$name",
           snackPosition: SnackPosition.BOTTOM);
